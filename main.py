e86921ee0e90701f0ee32b5af89444190188f904
Anaël <anael@barodine.fr>
2026-09-14 14:40:40 +0200

    fix: Enregistrement aux bons endroits.

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ if __name__ == "__main__":
                         if not os.path.exists(f"{series_output_dir}{title} ({year})/Season {season_no}"):
                             os.mkdir(f"{series_output_dir}{title} ({year})/Season {season_no}")
 
-                        success = process(f"{input_dir}{dir_name}/{file_name}", f"{series_output_dir}{title} ({year})/" + "Specials/" if season_no == "00" else "Season {season_no}/", temp_dir, title, year, original_language, season_no, episode_no, to_episode_no, force_av1 = force_av1, mkvtools_path = mkvtools_path)
+                        success = process(f"{input_dir}{dir_name}/{file_name}", f"{series_output_dir}{title} ({year})/" + ("Specials/" if season_no == "00" else f"Season {season_no}/"), temp_dir, title, year, original_language, season_no, episode_no, to_episode_no, force_av1 = force_av1, mkvtools_path = mkvtools_path)
                         if success:
                             os.rename(f"{input_dir}{dir_name}/{file_name}", f"{input_dir}{dir_name}/DONE - {file_name}")
                         else:
