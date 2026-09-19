8d820bb41e08132912234ecd12038afb14ef3c5f
Anaël <anael@barodine.fr>
2026-09-19 20:01:56 +0200

    feat: Camembert à l'analyse

--- helpers/check_metric.py
+++ helpers/check_metric.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import tqdm
+import matplotlib.pyplot as plt
 
 
 MOVIES_DIR = "/Volumes/videos/Movies/"
@@ -13,12 +14,14 @@ BOLD = "\033[1m"
 END = "\033[0m"
 
 def main():
-    total_files = len(os.listdir(MOVIES_DIR)) + sum(len(os.listdir(f"{SERIES_DIR}{dir}/")) for dir in os.listdir(SERIES_DIR) if not dir.startswith("."))
+    movies_count = len(os.listdir(MOVIES_DIR))
+    episodes_count = sum(sum(len(os.listdir(f"{SERIES_DIR}{dir}/{season}/")) for season in os.listdir(f"{SERIES_DIR}{dir}/") if not season.startswith(".")) for dir in os.listdir(SERIES_DIR) if not dir.startswith("."))
+    total_files = movies_count + episodes_count
     progress_bar = tqdm.tqdm(total = total_files, unit = "file(s)")
 
     movies = []
     for file in os.listdir(MOVIES_DIR):
-        if file.startswith("."):
+        if os.path.isdir(f"{MOVIES_DIR}{file}") or file.startswith("."):
             progress_bar.update()
             continue
         else:
@@ -39,30 +42,34 @@ def main():
 
     series = []
     for dir in os.listdir(SERIES_DIR):
-        if dir.startswith("."):
+        if os.path.isfile(f"{SERIES_DIR}{dir}") or dir.startswith("."):
             progress_bar.update()
             continue
         else:
             episodes = []
-            for file in os.listdir(f"{SERIES_DIR}{dir}/"):
-                if file.startswith("."):
+            for season in os.listdir(f"{SERIES_DIR}{dir}/"):
+                if os.path.isfile(f"{SERIES_DIR}{dir}/{season}") or season.startswith("."):
                     continue
                 else:
-                    file_path = f"{SERIES_DIR}{dir}/{file}"
-                    #print(subprocess.run(["ffprobe", "-v", "error", "-select_streams", "v:0", file_path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout)
-                    codec, seconds = str(subprocess.run(["ffprobe", "-v", "error", "-select_streams", "v:0", "-show_entries", "stream=codec_name:format=duration", "-of", "default=noprint_wrappers=1:nokey=1",file_path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout, encoding = "UTF-8").split("\n")[:2]
-                    seconds = float(seconds)
-                    size = os.path.getsize(file_path)
-                    metric = round((size / 1e9) / (seconds / 3600), 2)
-                    episodes.append({
-                        "title": file,
-                        "codec": codec,
-                        "size": size,
-                        "duration": seconds,
-                        "metric": metric
-                    })
-                #print(f"OK - {file}")
-                progress_bar.update()
+                    for file in os.listdir(f"{SERIES_DIR}{dir}/{season}/"):
+                        if os.path.isdir(f"{SERIES_DIR}{dir}/{season}/{file}") or file.startswith("."):
+                            continue
+                        else:
+                            file_path = f"{SERIES_DIR}{dir}/{season}/{file}"
+                            #print(subprocess.run(["ffprobe", "-v", "error", "-select_streams", "v:0", file_path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout)
+                            codec, seconds = str(subprocess.run(["ffprobe", "-v", "error", "-select_streams", "v:0", "-show_entries", "stream=codec_name:format=duration", "-of", "default=noprint_wrappers=1:nokey=1",file_path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout, encoding = "UTF-8").split("\n")[:2]
+                            seconds = float(seconds)
+                            size = os.path.getsize(file_path)
+                            metric = round((size / 1e9) / (seconds / 3600), 2)
+                            episodes.append({
+                                "title": file,
+                                "codec": codec,
+                                "size": size,
+                                "duration": seconds,
+                                "metric": metric
+                            })
+                        #print(f"OK - {file}")
+                        progress_bar.update()
 
             seconds = sum(episode["duration"] for episode in episodes)
             size = sum(episode["size"] for episode in episodes)
@@ -77,14 +84,28 @@ def main():
 
     progress_bar.close()
 
-    for movie in sorted(movies + series, key = lambda m: m["metric"]):
-        print(f"{GREEN if movie['metric'] < 1.2 else YELLOW if movie['metric'] < 2 else RED}{movie['metric']:0<4} Go/h{END} {GREEN if movie['codec'] == 'av1' else YELLOW if movie['codec'] == 'hevc' else RED if movie['codec'] == 'h264' else ''}({movie['codec']:<4}){END} - {round(movie['size'] / 1e9, 2)}Go/{int(movie['duration'] // 3600)}h{(int(movie['duration'] % 3600) // 60):0>2} - {movie['title']}")
-    total_size = sum(movie["size"] for movie in movies + series)
-    total_duration = sum(movie["duration"] for movie in movies + series)
+    shows = movies + series
+
+    # Console Display
+    shows.sort(key = lambda m: m["metric"])
+    for show in shows:
+        print(f"{GREEN if show['metric'] < 1.2 else YELLOW if show['metric'] < 2 else RED}{show['metric']:0<4} Go/h{END} {GREEN if show['codec'] == 'av1' else YELLOW if show['codec'] == 'hevc' else RED if show['codec'] == 'h264' else ''}({show['codec']:<4}){END} - {round(show['size'] / 1e9, 2)}Go/{int(show['duration'] // 3600)}h{(int(show['duration'] % 3600) // 60):0>2} - {show['title']}")
+    total_size = sum(show["size"] for show in shows)
+    total_duration = sum(show["duration"] for show in shows)
     total_metric = round((total_size / 1e9) / (total_duration / 3600), 2)
     print(f"--> Total size of everything: {round(total_size / 1e9, 2)} Go")
     print(f"--> Total duration of everything: {int(total_duration // 3600)}h{(int(total_duration % 3600) // 60):0>2} ({total_duration} seconds)")
     print(f"--> Mean metric of everything: {total_metric} Go/h")
+
+    # Camembert
+    shows.sort(key = lambda m: m["size"], reverse = True)
+    fig, ax = plt.subplots()
+    ax.pie(
+        list(map(lambda m: m["size"], shows)),
+        labels = [m["title"] if i < 30 else "" for i, m in enumerate(shows)],
+        colors = [("#1aff1a" if i % 2 == 0 else "#00e600") if m["codec"] == "av1" else ("#ffff1a" if i % 2 == 0 else "#e5e600") if m["codec"] == "hevc" else ("#ff1a1a" if i % 2 == 0 else "#e60000") if m["codec"] == "h264" else ("#8c8c8c" if i % 2 == 0 else "#737373") for i, m in enumerate(shows)]
+    )
+    plt.show()
 
 if __name__ == "__main__":
     main()
